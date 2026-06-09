2981a97d773ee691093b1966920df272ef9ccc97
Franck Cornu <franck.cornu@outlook.com>
2026-06-09 15:17:42 -0400

    fix: updated template

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -8,16 +8,13 @@ import dotenv from "dotenv";
 // Load .env values
 dotenv.config({ path: '.env.docusaurops' });
 
-const azureSiteUrlEnvName = `ENV_AZURE_${'TECH' + 'HUB'}_SITE_URL`;
-const azureSiteUrl = process.env[azureSiteUrlEnvName as string];
-
 const config: Config = {
   title: process.env.ENV_SITE_TITLE ? process.env.ENV_SITE_TITLE : 'DocusaurOps Project documentation template ',
   tagline: process.env.ENV_SITE_TAGLINE ? process.env.ENV_SITE_TAGLINE : 'Project tagline',
   favicon: 'img/favicon.ico',
   
   // Set the production url of your site here
-  url: azureSiteUrl ? azureSiteUrl : 'http://localhost:3000',
+  url: process.env.ENV_AZURE_DOCUSAUROPS_SITE_URL ? process.env.ENV_AZURE_DOCUSAUROPS_SITE_URL : 'http://localhost:3000',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: process.env.ENV_BASE_URL ? process.env.ENV_BASE_URL : '/',
@@ -71,16 +68,16 @@ const config: Config = {
           label: 'How to use this template',
         },
         {
-          href: 'https://github.com/your-org/docusaurops-template',
+          href: '[[ENV_DOCUSAUROPS_HOST_REPO]]',
           position: 'right',
           html: `
-              <a style="display:flex" href="https://github.com/your-org/docusaurops-template" target="_blank" rel="noreferrer noopener" aria-label="Components playground">
+              <a style="display:flex" href="[[ENV_DOCUSAUROPS_HOST_REPO]]" target="_blank" rel="noreferrer noopener" aria-label="Components playground">
                 <svg id="f4337506-5d95-4e80-b7ca-68498c6e008e" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18"><defs><linearGradient id="ba420277-700e-42cc-9de9-5388a5c16e54" x1="9" y1="16.97" x2="9" y2="1.03" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#0078d4" /><stop offset="0.16" stop-color="#1380da" /><stop offset="0.53" stop-color="#3c91e5" /><stop offset="0.82" stop-color="#559cec" /><stop offset="1" stop-color="#5ea0ef" /></linearGradient></defs><title>DevOps organization</title><path id="a91f0ca4-8fb7-4019-9c09-0a52e2c05754" d="M17,4v9.74l-4,3.28-6.2-2.26V17L3.29,12.41l10.23.8V4.44Zm-3.41.49L7.85,1V3.29L2.58,4.84,1,6.87v4.61l2.26,1V6.57Z" fill="url(#ba420277-700e-42cc-9de9-5388a5c16e54)" /></svg>
               </a>
             `
         },
         {
-          href: azureSiteUrl ? azureSiteUrl : '/',
+          href: process.env.ENV_AZURE_DOCUSAUROPS_SITE_URL ? process.env.ENV_AZURE_DOCUSAUROPS_SITE_URL : '/',
           label: 'DocusaurOps Home',
           position: 'right',
         }
@@ -146,9 +143,8 @@ const config: Config = {
               },
               plugins: [
                 new webpack.EnvironmentPlugin({
-                  ENV_AZURE_DOCUSAUROPS_SITE_URL: azureSiteUrl ? azureSiteUrl : 'http://localhost:3000',
+                  ENV_AZURE_DOCUSAUROPS_SITE_URL: process.env.ENV_AZURE_DOCUSAUROPS_SITE_URL ? process.env.ENV_AZURE_DOCUSAUROPS_SITE_URL : 'http://localhost:3000',
                   ENV_BASE_URL: process.env.ENV_BASE_URL ? process.env.ENV_BASE_URL : '/',
-                  ENV_SEARCH_URL: process.env.ENV_SEARCH_URL ? process.env.ENV_SEARCH_URL : 'http://localhost:3000'
                 })
               ]
             };
